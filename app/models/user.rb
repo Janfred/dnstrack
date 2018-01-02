9a6771ee0b5966fd0a94a36ee38ea7b2dd18c73e
Jan-Frederik Rieckers <rieckers@uni-bremen.de>
2018-01-02 17:35:14 +0100

    Make registerable and recoverable configurable

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable, :confirmable,
-         :recoverable, :rememberable, :trackable, :validatable,
+  devise :database_authenticatable, :confirmable,
+         :rememberable, :trackable, :validatable,
          :lockable, :authentication_keys => [:username]
+  devise :registerable if Settings.users.registerable
+  devise :recoverable if Settings.users.recoverable
 end
